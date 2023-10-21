c779f18fa8a7dc9fce6380c28e80581a4e7af95e
Danilonbs <danilo_eu_sk8@hotmail.com>
2023-10-21 18:18:49 -0300

    input header

--- capputeeno/src/app/components/header.tsx
+++ capputeeno/src/app/components/header.tsx
@@ -1,6 +1,7 @@
 "use client"
 import styled from "styled-components"
 import { Saira_Stencil_One } from 'next/font/google'
+import { PrimaryInput } from "./primary-input"
 
 const sairaStencil = Saira_Stencil_One({
   weight: ['400'],
@@ -29,7 +30,9 @@ export function Header (props: HeaderProps) {
   return (
     <TagHeader>
       <Logo className={sairaStencil.className}>Capputeeno</Logo>
-      <div></div>
+      <div>
+        <PrimaryInput placeholder="Procurando por algo específico?" />
+      </div>
     </TagHeader>
   )
 }
